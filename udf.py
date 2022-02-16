e06a84a7071878ca02b0b5eeab793e3d25ce0bdd
Lauradaley96 <lauradaley96@outlook.com>
2022-02-16 10:24:30 +0000

    first commit

---
+++ udf.py
@@ -0,0 +1,13 @@
+def msg():
+    print("--------")
+    print("Hello everyone")
+    print("--------")
+
+def friendly():
+    print("I'm so happy you're here!")
+    print("Hooray!")
+
+print("Receiving a message...")
+msg()
+print("And another one...")
+friendly()
